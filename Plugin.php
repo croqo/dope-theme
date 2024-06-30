3134ed3a07fed6eeb38947c2af5d26e6c462dfba
Croqo <croqo@outlook.com>
2024-06-30 20:00:32 +0300

    Initiagram
    Signed-off-by: Croqo <croqo@outlook.com>

---
+++ Plugin.php
@@ -0,0 +1,74 @@
+<?php namespace Croqo\Gram;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Plugin Information File
+ *
+ * @link https://docs.octobercms.com/3.x/extend/system/plugins.html
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * register method, called when the plugin is first registered.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * boot method, called right before the request route.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * registerComponents used by the frontend.
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Croqo\Gram\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * registerPermissions used by the backend.
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'croqo.gram.some_permission' => [
+                'tab' => 'Gram',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * registerNavigation used by the backend.
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'gram' => [
+                'label' => 'Gram',
+                'url' => Backend::url('croqo/gram/mycontroller'),
+                'icon' => 'icon-leaf',
+                'permissions' => ['croqo.gram.*'],
+                'order' => 500,
+            ],
+        ];
+    }
+}
